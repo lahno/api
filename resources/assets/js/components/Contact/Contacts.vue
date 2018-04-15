2b83641e9e56e7d227f1af35fb5d84eb4cdfa0e5
lahno <lahno.dima@gmail.com>
2018-04-16 01:43:32 +0300

    add preloader

--- resources/assets/js/components/Contact/Contacts.vue
+++ resources/assets/js/components/Contact/Contacts.vue
@@ -45,11 +45,36 @@
                     font-weight: normal
                 div
                     margin: 0 auto
+    .table_block
+        position: relative
+        z-index: 5
+        .loader_block
+            width: 100%
+            height: 100%
+            .loader
+                position: absolute
+                top: 50%
+                left: 50%
+                z-index: 15
+                margin-left: -30px
+                margin-top: -30px
+                border: 2px solid #d9edf7
+                border-top: 6px solid #3498db
+                border-radius: 50%
+                width: 60px
+                height: 60px
+                animation: spin 1s linear infinite
+            @keyframes spin
+                0%
+                    transform: rotate(0deg)
+                100%
+                    transform: rotate(360deg)
+        .table_load.active
+            opacity: 0.4
 </style>
 <template>
     <div class="contacts_list panel panel-default">
         <div class="panel-heading">Contacts</div>
-
         <div class="panel-body">
             <div class="alert alert-info" v-if="message">
                 {{ message }}
@@ -69,20 +94,28 @@
                     <switches v-model="online" theme="bulma" color="green" textDisabled="Update offline" textEnabled="Update online"/>
                 </div>
             </div>
-            <datatable :columns="columns" :sortKey="sortKey" :sortOrders="sortOrders" @sort="sortBy">
-                <tbody>
-                <tr v-for="contact in contacts" :key="contact.id" class="table_tr" v-bind:class="{ success: contact.new}">
-                    <td>{{contact.id}}</td>
-                    <td>{{contact.firstname}}</td>
-                    <td>{{contact.phone}}</td>
-                    <td>{{contact.email}}<span class="label label-primary label-new" v-if="contact.new">New</span></td>
-                    <td>{{contact.city}}</td>
-                    <td>
-                        <button type="button" class="btn btn-link btn-xs" @click="edit(contact)"><span class="glyphicon glyphicon-search"></span></button>
-                    </td>
-                </tr>
-                </tbody>
-            </datatable>
+            <div class="table_block">
+                <div class="loader_block" v-show="loader">
+                    <div class="loader"></div>
+                </div>
+                <div class="table_load" v-bind:class="{ active: loader }">
+                    <datatable :columns="columns" :sortKey="sortKey" :sortOrders="sortOrders" @sort="sortBy">
+                        <tbody>
+                        <tr v-for="contact in contacts" :key="contact.id" class="table_tr" v-bind:class="{ success: contact.new}">
+                            <td>{{contact.id}}</td>
+                            <td>{{contact.firstname}}</td>
+                            <td>{{contact.phone}}</td>
+                            <td>{{contact.email}}<span class="label label-primary label-new" v-if="contact.new">New</span></td>
+                            <td>{{contact.city}}</td>
+                            <td>
+                                <button type="button" class="btn btn-link btn-xs" @click="edit(contact)"><span class="glyphicon glyphicon-search"></span></button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </datatable>
+                </div>
+            </div>
+
             <pagination :pagination="pagination"
                         @prev="getContacts(pagination.prevPageUrl)"
                         @next="getContacts(pagination.nextPageUrl)">
@@ -192,11 +225,13 @@ export default {
                 contact_form: []
             },
             online: true,
+            loader: true,
             views: [10, 20, 30, 50, 100]
         }
     },
     methods: {
         getContacts(url = '/api/get_contacts') {
+            this.loader = true;
             this.tableData.draw++;
             axios.get(url, {params: this.tableData})
                 .then(response => {
@@ -204,6 +239,7 @@ export default {
                     if (this.tableData.draw == data.draw) {
                         this.contacts = data.data.data;
                         this.configPagination(data.data);
+                        this.loader = false;
                     }
                 })
                 .catch(errors => {
