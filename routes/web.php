f09788083af3cdb50f1dbb68c8fdc27664bf8499
lahno <lahno.dima@gmail.com>
2018-04-16 19:36:19 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,6 @@ Route::group(['middleware' => 'auth'], function () {
 /* Log view */
 Route::get('l_v_', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 
-Route::post('phpInfo', function (){
+Route::get('phpInfo', function (){
    phpinfo();
 });
